07500304bedcf626ce1fdca9763ff74abb59c022
karankumar4060 <mr.karankumar4060@gmail.com>
2026-02-22 21:42:17 +0530

    gready practice

---
+++ script.js
@@ -0,0 +1,47 @@
+// console.log("Hello World");
+// console.log("im karan");
+
+
+// let a = 10;
+// let b = 20;
+// // let sum = a + b;
+// // let output="The sum of a and b is: " +(a+b) +"amount";
+// // console.log(output);
+
+// ********************************************************************************************
+// // TEMPLATE LITERAL
+
+
+// let output =`the total : ${a+b} rupee`;
+// console.log(output);
+// ***********************************************************************************
+
+
+// PRACTICE QUESTION 1
+
+// let col='green';
+// if(col==='red'){
+//     console.log("stop");
+// }else if(col==="yellow"){
+//     console.log("wait");
+// }else{
+//     console.log("go");
+// }
+
+
+// *********************************************************************************
+
+
+// PRACTICE QUESTION 2
+
+// let str="axkaran";
+// if(str[0]=="a" && str.length > 3){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+
+// **********************************************************************************
+
+
